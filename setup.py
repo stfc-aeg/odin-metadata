b509096720353f35b39e4890fb7600cd6f5adc2d
Ashley Neaves <ashley.neaves@stfc.ac.uk>
2021-05-07 15:19:22 +0100

    Fixed reference to Odin Control in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 import versioneer
 
 install_requires = [
-    'odin @ git+https://github.com/odin-detector/odin-control@master#egg=odin-control',
+    'odin-control @ git+https://github.com/odin-detector/odin-control@master#egg=odin-control',
     'pyzmq>=22.0',
     'h5py'
 ]
